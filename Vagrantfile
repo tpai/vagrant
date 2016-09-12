3a5746156524d472fac84ac0c3ae92b2842bc4b5
tonypai <tony77794@gmail.com>
2016-09-12 15:19:12 +0800

    Simplify ASAP

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,10 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu"
+  config.vm.box = "ubuntu/trusty64"
   config.vm.network "forwarded_port", guest: 80, host: 8000
   config.vm.network "private_network", ip: "10.10.10.10"
   config.vm.synced_folder "www", "/usr/share/nginx/html", type: "nfs"
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "4096"
   end
-  config.vm.provision "shell", path: "install.sh"
+  config.vm.provision "shell", path: "install.sh", privileged: true
 end
--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ curl
 vim
 git
 nginx
-zsh
 "
 
 PACKAGES=""
@@ -17,47 +16,14 @@ done
 
 IS_INSTALLED=".installed"
 
-VIM_CONFIG="
-\" Terminal Color
-let g:solarized_termcolors = 256
-
-set mouse=a                     \"Mouse Enabled
-set clipboard=unnamed           \"Copy To System Clipboard
-
-set shiftwidth=4
-set softtabstop=4
-set tabstop=4
-set expandtab
-"
-
 if [ ! -e $IS_INSTALLED ];then
     touch $IS_INSTALLED
 
     echo "Package Installation Initiated..."
 
-    # Install Packages
+    # Install packages
     sudo apt-get update
     sudo apt-get install -y $PACKAGES
-
-    # Install NVM
-    sh -c "`curl -fsSL https://raw.github.com/creationix/nvm/master/install.sh`"
-
-    # This enables NVM without reopen terminal
-    export NVM_DIR="/home/vagrant/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-    # Install Node and Set Alias
-    nvm install v4.2.4
-    nvm alias default v4.2.4
-    nvm use v4.2.4
-    npm install -g npm
-
-    # Install on-my-zsh & change shell
-    sh -c "`curl -fsSL http://cdn.rawgit.com/robbyrussell/oh-my-zsh/master/tools/install.sh`"
-    sudo chsh -s /bin/zsh vagrant
-
-    # Customize vim settings
-    echo "$VIM_CONFIG" > "/home/vagrant/.vimrc"
 fi
 
 sudo service nginx restart
