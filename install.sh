03d975cefdeeb87582c337d80c1a2115ee6f3386
tonypai <tony77794@gmail.com>
2016-09-13 11:43:46 +0800

    Add mongodb, nvm and nodemon

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ curl
 vim
 git
 nginx
+mongodb-org
 "
 
 PACKAGES=""
@@ -21,9 +22,29 @@ if [ ! -e $IS_INSTALLED ];then
 
     echo "Package Installation Initiated..."
 
-    # Install packages
+    # MongoDB Required Settings
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+    # Install Packages
     sudo apt-get update
     sudo apt-get install -y $PACKAGES
+
+    # Fix No User Set Locale For Mongo Client
+    sudo locale-gen
+
+    # Node Installation
+    export NVM_DIR="/home/vagrant/.nvm" && (
+        git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+        cd "$NVM_DIR"
+        git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+    ) && . "$NVM_DIR/nvm.sh"
+
+    nvm install v4.5.0
+    nvm ls
+    npm install -g npm nodemon
+
 fi
 
 sudo service nginx restart
+sudo service mongod restart
